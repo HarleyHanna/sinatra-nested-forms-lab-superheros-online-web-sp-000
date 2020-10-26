40391f24a663da1e5f570199e69cd077095c96a3
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-26 15:37:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do
+      erb :'../../views/new_team'
+    end 
+    
+    post '/teams' do
+      @teams = params
+      
+      
+      erb :'../../views/teams'
+    end 
+    
+    
 
 
 end
